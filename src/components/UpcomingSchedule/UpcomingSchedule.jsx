cb6c313cb883bf0a95f07bf4f4061791327ec113
harikrishna7730 <abhitangala@gmail.com>
2025-05-25 19:50:13 +0530

    added

--- src/components/UpcomingSchedule/UpcomingSchedule.jsx
+++ src/components/UpcomingSchedule/UpcomingSchedule.jsx
@@ -9,24 +9,24 @@ const UpcomingSchedule = () => {
          <p className='day'>On Thursday</p>
          <div className='schedule-card'>
             <div className='schedule-section-card'>
-                   <div> <span className='card-title'>Health checkup complete<span  className='schedule-emoji'> 💉 </span></span> </div>
+                   <div> <span id='card-title'>Health checkup complete<span  className='schedule-emoji'> 💉 </span></span> </div>
                 <div><span>11:00 AM</span></div>
             </div>
              <div className='schedule-section-card'>
-                  <div> <span className='card-title'>Ophthalmologist<span  className='schedule-emoji'> 👁️ </span></span> </div>
+                  <div> <span id='card-title'>Ophthalmologist<span  className='schedule-emoji'> 👁️ </span></span> </div>
                 <div><span>14:00 PM</span></div>
             </div>
          </div>
          </div>
          <div>
-         <p className='day'>On Thursday</p>
+         <p className='day'>On Saturday</p>
          <div className='schedule-card'>
             <div className='schedule-section-card'>
-                <div> <span className='card-title'>Cardiologist<span  className='schedule-emoji'> 🙎‍♂️ </span></span> </div>
+                <div> <span id='card-title'>Cardiologist<span  className='schedule-emoji'> 🙎‍♂️ </span></span> </div>
                 <div><span>12:00 AM</span></div>
             </div>
              <div className='schedule-section-card'>
-                <div> <span className='card-title'>Neurologist<span  className='schedule-emoji'> 🙎‍♂️ </span></span> </div>
+                <div> <span id='card-title'>Neurologist<span  className='schedule-emoji'> 🙎‍♂️ </span></span> </div>
                 <div><span>16:00 PM</span></div>
             </div>
          </div>
