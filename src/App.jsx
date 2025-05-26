e4b66cfb9b30764232555a67d47962b297118380
TridiziDev3 <tridizi.dev3@gmail.com>
2025-05-26 13:01:18 +0530

    dahboard body

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import './App.css'
-import AsideBar from './components/Asidebar/AsideBar'
-import CalenderView from './components/CalenderView/CalenderView'
-import Navbar from './components/Navbar/Navbar'
-import UpcomingSchedule from './components/UpcomingSchedule/UpcomingSchedule'
+import "./App.css";
+import AsideBar from "./components/Asidebar/AsideBar";
+import BodyView from "./components/bodyView/BodyView";
+import CalenderView from "./components/CalenderView/CalenderView";
+import Navbar from "./components/Navbar/Navbar";
+import UpcomingSchedule from "./components/UpcomingSchedule/UpcomingSchedule";
 
 function App() {
   return (
     <>
-    <div className='App'>
-      <div className='section-1'>
-           <AsideBar/>
+      <div className="App">
+        <div className="section-1">
+          <AsideBar />
+        </div>
+        <div className="section-2">
+          <Navbar />
+          <BodyView />
+        </div>
+
+        <div className="section-3">
+          <div>
+            <CalenderView />
+          </div>
+          <div>
+            <UpcomingSchedule />
+          </div>
+        </div>
       </div>
-       <div className='section-2'>
-          <Navbar/>
-       </div>
-    
-    <div className='section-3'>
-      <div>
-         <CalenderView/>
-      </div>
-      <div>
-        <UpcomingSchedule/>
-      </div>
-    </div>
-    </div>
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
